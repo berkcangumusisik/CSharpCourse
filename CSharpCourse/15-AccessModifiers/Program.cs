7897907b737ef8b05c2c68168931481f596dd8fd
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-11-24 23:47:25 +0300

    Referans ve Değer Tip Eklendi

---
+++ CSharpCourse/15-AccessModifiers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _15_AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer
+    {
+        protected int id { get; set; }
+        public void Save()
+        {
+            
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+
+    class Student:Customer
+    {
+        public void Save2()
+        {
+            
+        }
+    }
+
+    internal class Course
+    {
+        public string Name { get; set; }
+    }
+}
+
+/*
+ * private sadece tanımlandığı blok içinde kullanım sağlar.
+ * protected'ın private farkı inheritence yapılan yerde kullanılabilir.
+ * internal referans ihtiyacı olmadan kullanılır. Aynı assembly üzerinde olmalıdır.
+ * public farklı projede kullanım imkanı sağlar. Referans ile çalışır.
+ */
